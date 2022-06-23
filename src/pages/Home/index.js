2068655b77c6f965f8352b3294c632b65a2276cd
Carlos Tiago Silva Costa <95192387272@detran.ms.gov.br>
2022-06-23 16:29:17 -0300

    atualizações do projeto agencia de viagens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState }from 'react';
+import { useDispatch } from 'react-redux';
+import { addReserve } from '../../store/modules/reserve/actions';
 import { MdFlightTakeoff } from 'react-icons/md';
 import api from '../../services/api';
 import './style.css';
 
 export default function Home() {
-
+  const dispatch = useDispatch();
   const [trips, settrips] = useState([]);
 
   useEffect(() => {
@@ -19,6 +21,10 @@ export default function Home() {
 
   }, []);
 
+  function handleAdd(trip){
+    dispatch(addReserve(trip));
+  }
+
  return (
    <div>
         <div className="box"> 
@@ -28,7 +34,7 @@ export default function Home() {
               <strong>{trip.title}</strong>
               <span>Status: {trip.title ? 'Disponível' : 'Indisponível'}</span>
 
-              <button type="button" onClick={() => {}}>
+              <button type="button" onClick={() => handleAdd(trip) }>
                 <div>
                   <MdFlightTakeoff size={16} color="#FFF" />
                 </div>
--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeReserve } from '../../store/modules/reserve/actions';
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
+import './style.css';
 
 export default function Reservas() {
+  const dispatch = useDispatch();
+  const reserves = useSelector(state => state.reserve);
+
+  function handleRemove(id){
+    dispatch(removeReserve(id));
+  }
+
+  
+
  return (
    <div>
-        <h1>Reservas</h1>
+        <h1 className="title">Você solicitou {reserves.length} reserva</h1>
+
+        {reserves.map(reserve => (
+            <div className="reservas" key={reserve.id}>
+              <img src={reserve.image} alt={reserve.title} />
+              <strong>{reserve.title}</strong>
+
+              <div>
+                <button type="button" onClick={() => {}}>
+                  <MdRemoveCircle size={20} color="#191919" />
+                </button>
+                <input type="text" readOnly value={reserve.amount} />
+                <button type="button" onClick={() => {}}>
+                  <MdAddCircle size={20} color="#191919" />
+                </button>
+              </div>
+
+              <button type="button" onClick={() => handleRemove(reserve.id)} >
+                <MdDelete size={20} color="#191919" />
+              </button>
+    
+          </div>
+        ))}      
+
+      <footer>
+        <button type="button">Solicitar Reserva</button>
+      </footer>
+   
    </div>
+   
  );
 }
